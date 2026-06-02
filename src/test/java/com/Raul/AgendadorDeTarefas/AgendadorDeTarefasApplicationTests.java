de67567af83b73f6bb8da86b732003d8bf048070
Raulvini <rubenserich942@gmail.com>
2026-06-02 16:39:57 -0300

    first commit

---
+++ src/test/java/com/Raul/AgendadorDeTarefas/AgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Raul.AgendadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
